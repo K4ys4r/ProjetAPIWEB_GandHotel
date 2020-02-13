309184ca7a4d7a1754d8a8bda185c0c046d2e8bb
PChaig <pichaigneau@laposte.net>
2020-02-13 09:43:29 +0100

    RequeteSQL

---
+++ RequestsSQL.sql
@@ -0,0 +1,3 @@
+﻿RESTORE DATABASE GrandHotel FROM DISK = 'C:\Users\Adminl\Desktop\Projet Web API\GrandHotel.bak' WITH REPLACE,
+MOVE 'GrandHotel' TO 'C:\Users\Adminl\Desktop\Projet Web API\GrandHotel.mdf',
+MOVE 'GrandHotel_log' TO 'C:\Users\Adminl\Desktop\Projet Web API\GrandHotel.ldf'
