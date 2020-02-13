c533b7795b040877ff37b5d6d92f8a9fbbc30e4b
PChaig <pichaigneau@laposte.net>
2020-02-13 18:52:47 +0100

    SQLv3

--- RequestsSQL.sql
+++ RequestsSQL.sql
@@ -7,7 +7,7 @@ MOVE 'GrandHotel_log' TO 'C:\Users\Adminl\Desktop\Projet Web API\GrandHotel.ldf'
 
 select c.Id, c.Nom, c.Prenom from Client c
 inner join Telephone t on t.IdClient= c.Id
-where t.Numero like ('06%') or t.Numero like ('07%')
+where t.CodeType = 'M'
 
 
 --2.	Le taux moyen de réservation de l’hôtel par mois-année (2015-01, 2015-02…), c'est à dire la moyenne sur les chambres du ratio (nombre de jours de réservation dans le mois / nombre de jours du mois)
@@ -35,6 +35,7 @@ order by year(cal.Jour) desc
 
 
 --4.	Le chiffre d’affaire de l’hôtel par trimestre de chaque année
+-- 9 lignes
 
 select R.Annee, R.Trimestre, sum(R.CA_Mois) CA_Trimestre from
 (select  year(f.DatePaiement) Annee,
@@ -44,7 +45,7 @@ when month(f.DatePaiement) between 4 and 6  then 'T2'
 when month(f.DatePaiement) between 7 and 9  then 'T3'
 when month(f.DatePaiement) between 9 and 12  then 'T4'
 end as Trimestre,
-sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) CA_Mois
+sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite)) CA_Mois
 from Facture f
 inner join LigneFacture lf on f.Id=lf.IdFacture
 group by year(f.DatePaiement),month(f.DatePaiement)
@@ -56,13 +57,13 @@ order by R.Annee,R.Trimestre
 
 select R.Tranche_CA, count(R.IdClient) as NbClients
 from
-(select IdClient, sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) CA_Client,
+(select IdClient, sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite)) CA_Client,
 case
-when sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) < 5000  then 'Tranche < 5000'
-when sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) between 5000 and 6000  then '5000 >= Tranche > 6000'
-when sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) between 6000 and 7000  then '6000 >= Tranche > 7000'
-when sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) between 7000 and 8000  then '7000 >= Tranche > 8000'
-when sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) >=8000  then 'Tranche >= 8000'
+when sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite)) < 5000  then 'Tranche < 5000'
+when sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite)) between 5000 and 6000  then '5000 >= Tranche > 6000'
+when sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite)) between 6000 and 7000  then '6000 >= Tranche > 7000'
+when sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite)) between 7000 and 8000  then '7000 >= Tranche > 8000'
+when sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite)) >=8000  then 'Tranche >= 8000'
 end as Tranche_CA
 from Facture f
 inner join LigneFacture lf on f.Id=lf.IdFacture
@@ -70,17 +71,57 @@ inner join Client c on c.Id= f.IdClient
 group by IdClient) as R
 group by R.Tranche_CA
 
-select IdClient, sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite))
+select IdClient, sum((lf.MontantHT*(1-lf.TauxReduction)*(1+lf.TauxTVA)*lf.Quantite))
 from Facture f
 inner join LigneFacture lf on f.Id=lf.IdFacture
 group by IdClient
 having sum((lf.MontantHT*(1-lf.TauxReduction+lf.TauxTVA)*lf.Quantite)) < 5000 
 
 --6.	Code T-SQL pour augmenter à partir du 01/01/2019 les tarifs des chambres de type 1 de 5%, et ceux des chambres de type 2 de 4% par rapport à l'année précédente
+drop procedure if exists EvolutionPrix 
+go
+create procedure EvolutionPrix @taux decimal, @date date
+as
+begin
+insert Tarif (Code,DateDebut,Prix) values (('CHB1-'+convert(nvarchar,year(@date))),(@date),(0))
 
+set @taux = 0.05
+update Tarif set Code='CHB1-'+convert(nvarchar,year(@date)), DateDebut=(@date),Prix=((1+@taux)*Prix)
+insert Tarif (Code,DateDebut,Prix) values (('CHB2-'+convert(nvarchar,year(@date))),(@date),(0))
 
+update Tarif set Code='CHB2-'+convert(nvarchar,year(@date)), DateDebut=(@date),Prix=((1+@taux)*Prix)
+set @taux = 0.04
+end 
+go
+
+create procedure EvolutionPrix @date date, @prix decimal, @type int, @taux decimal
+as
+begin
+declare @nvprix as decimal = (1.0+@taux)*@prix
+insert Tarif (Code,DateDebut,Prix) values (('CHB'+convert(nvarchar,@type)+'-'+convert(nvarchar,year(@date))),(@date),(@nvprix))
+end 
+go
+
+begin tran
+declare @date as date= dateadd(year,1,(select top(1) DateDebut from Tarif order by DateDebut desc)) 
+declare @prix as decimal= (select top(1) Prix from Tarif where Code like ('CHB1%') order by DateDebut desc )
+declare @type as int =1
+declare @taux as decimal =0.05
+exec EvolutionPrix @date,@prix,@type,@taux
+
+select * from Tarif 
+rollback tran
 
 --7.	Clients qui ont passé au total au moins 7 jours à l’hôtel au cours d’un même mois (Id, Nom, mois où ils ont passé au moins 7 jours)
-
+-- Stimac et Milionis
+select Id, Nom, format(r.Jour,'yyyy-MM') Mois from Client c
+inner join Reservation r on r.IdClient=c.Id
+group by format(r.Jour,'yyyy-MM'), Id, Nom
+having count(r.Jour)>=7
 
 --8.	Clients qui sont restés à l’hôtel au moins deux jours de suite au cours de l’année 2017
+--11 & 277 & 505
+select distinct(r.IdClient) from Reservation r
+where year(r.Jour) = 2017 and 1 in
+(select datediff(day,r.Jour,r2.Jour) from Reservation r2
+where year(r.Jour) = 2017 and r.IdClient = r2.IdClient)
